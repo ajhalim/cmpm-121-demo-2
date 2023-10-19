1760d0dc317748a306d09997e0b5f9d0847719c4
ajhalim <thenubbum@gmail.com>
2023-10-19 01:09:06 -0700

    Step 1 done

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
-
-const gameName = "Abe's Doodlers";
-
+const gameName = "Doodle Machine";
 document.title = gameName;
-
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 
+//Canvas stats
+const width = 256;
+const height = 256;
+
+const canvas = document.createElement("canvas");
+
+canvas.width = width;
+canvas.height = height;
+
 app.append(header);
+app.append(canvas);
