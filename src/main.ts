bb72c4fd72f338e35ae85e471351ad1a0b19d1cc
ajhalim <thenubbum@gmail.com>
2023-10-19 19:33:03 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,26 @@ const canvas = document.createElement("canvas");
 
 canvas.width = width;
 canvas.height = height;
+const ctx = canvas.getContext("2d");
+
+ctx!.fillStyle = "lightpink";
+
+ctx?.fillRect(0, 0, width, height);
+
+//const cursor = { active: false, x: 0, y: 0 };
+
+/* function draw(x, y, color) {}
+
+addEventListener("mousedown", (event) => {
+  cursor.active = true;
+  cursor.x = event.offsetX;
+  cursor.y = event.offsetY;
+});
+
+canvas.addEventListener("mousemove", (event) => {
+  if (cursor.active) {
+  }
+}); */
 
 app.append(header);
 app.append(canvas);
