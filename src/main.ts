6bdf16bb09c77e8e641392f76ab763628c7a4955
ajhalim <thenubbum@gmail.com>
2023-10-21 00:26:38 -0700

    part2 done

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
 import "./style.css";
 
+function draw(event: MouseEvent) {
+  if (drawing) {
+    ctx!.lineWidth = 1.5;
+    ctx!.lineCap = "round";
+    ctx!.strokeStyle = "black";
+
+    ctx!.lineTo(
+      event.clientX - canvas.offsetLeft,
+      event.clientY - canvas.offsetTop
+    );
+    ctx!.stroke();
+
+    ctx!.beginPath();
+    ctx!.moveTo(
+      event.clientX - canvas.offsetLeft,
+      event.clientY - canvas.offsetTop
+    );
+  }
+}
+
 const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Doodle Machine";
 document.title = gameName;
@@ -22,6 +42,38 @@ ctx?.fillRect(0, 0, width, height);
 
 //const cursor = { active: false, x: 0, y: 0 };
 
+//const paths = [];
+
+const clearButton: HTMLButtonElement = document.createElement("button");
+clearButton.innerText = "Clear";
+
+clearButton.addEventListener("click", () => {
+  ctx!.fillStyle = "lightpink";
+  ctx?.fillRect(0, 0, width, height);
+});
+
+let drawing = false;
+
+document.addEventListener("mousedown", () => {
+  console.log("down");
+  drawing = true;
+  ctx?.beginPath();
+});
+document.addEventListener("mouseup", () => {
+  console.log("up");
+  drawing = false;
+});
+canvas.addEventListener("mousemove", (e) => {
+  console.log("draw");
+  draw(e);
+});
+canvas.addEventListener("mouseenter", () => {
+  console.log("enter canvas");
+  ctx?.beginPath();
+});
+
+app.append(header, canvas, clearButton);
+
 /* function draw(x, y, color) {}
 
 addEventListener("mousedown", (event) => {
@@ -34,6 +86,3 @@ canvas.addEventListener("mousemove", (event) => {
   if (cursor.active) {
   }
 }); */
-
-app.append(header);
-app.append(canvas);
